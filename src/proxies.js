99b7a8cfe3125a1a5b5319530d6b22e7e82cc04d
avastel <antoine.vastel@gmail.com>
2022-03-30 19:35:29 +0200

    improve proxy ip verification

--- src/proxies.js
+++ src/proxies.js
@@ -23,7 +23,7 @@ class Proxy {
     }
 
     _isValidHost(ipAddress) {
-        return (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipAddress))
+        return net.isIP(ipAddress);
     }
 
     async _testConnection(timeout) {
